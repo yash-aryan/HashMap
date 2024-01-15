8c0f6b1e595f5fbb14dbbb6506f3e961a4c5ce4d
Yash <79582883+yash-aryan@users.noreply.github.com>
2024-01-15 23:54:38 +0530

    Add Several New Methods & Update Documentation
    - Added public HashMapFactory methods: hasEntry, removeEntry, getLength,
    getAllKeys, getAllEntries, clearAll.
    - Added private HashMapFactory method called `getAllFilledNodes` which
    returns all non-empty nodes in the hash map.
    It also inputs an optional callback function to format the node.
    - Updated README.md to add a useful and easy-to-read documentation.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Overview
 
-In this practice project, I tried creating a working HashMap Data Structure manually.
+-   In this practice project, I tried creating a working HashMap Data Structure manually.
+-   The hash function returns a hash number after parsing through all char of input string and multiplying a prime number to it's char code.
+-   The hash number is also converted to be able fit in the hash map using modulo operator.
+-   Intial Size of Hash Map = 16, but the size doubles each time current load surpasses the load factor (0.75).
+-   Each element/bucket of the hash map is a linked list, in case of collisions.
+-   Hash Map contains following methods:
+    -   `setEntry(key)`
+    -   `getValue(key)`
+    -   `hasEntry(key)`
+    -   `removeEntry(key)`
+    -   `getLength()`
+    -   `getAllKeys()`
+    -   `getAllEntries()`
+    -   `clearAll()`
+
+# Usage
+
+-   Get value using it's key
 
 ```js
 // Assigns Hash Map Object to 'map' variable.
@@ -9,5 +26,69 @@ const map = HashMapFactory();
 // Sets the key & value in the hash map internally.
 map.setEntry('fruit', 'banana');
 
-map.getValue('fruit'); // returns 'banana'
+map.hasEntry('fruit');
+> true
+
+map.getValue('fruit');
+> 'banana'
+```
+
+-   Overwrite old value of existing key
+
+```js
+const map = HashMapFactory();
+map.setEntry('animal', 'tiger');
+// New value with same key overwrites.
+map.setEntry('animal', 'elephant');
+
+map.getValue('animal');
+> 'elephant';
+```
+
+-   Get all keys/entries
+
+```js
+const map = HashMapFactory();
+map.setEntry('animal', 'cat');
+map.setEntry('age', 5);
+map.setEntry('likes', 'headpats');
+
+map.getAllKeys();
+> ['animal', 'age', 'likes'] // order may be different
+
+map.getAllEntries();
+> [['animal', 'cat'], ['age', 5], ['likes', 'headpats']] // order may be different
+
+map.getLength();
+> 3
+```
+
+-   Remove any entry using it's key
+
+```js
+const map = HashMapFactory();
+map.setEntry('animal', 'cat');
+map.getEntry('animal');
+> 'cat'
+
+map.removeEntry('animal');
+map.getEntry('animal');
+> null
+```
+
+-   Full Reset
+
+```js
+const map = HashMapFactory();
+map.setEntry('animal', 'cat');
+map.setEntry('age', 5);
+map.setEntry('likes', 'headpats');
+map.getLength();
+> 3
+
+map.clearAll();
+map.getLength();
+> 0
+map.getEntries();
+> []
 ```
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function HashMapFactory() {
 	const hashMap = Array(16),
 		loadFactor = 0.75;
-	let filledBuckets = 0;
+	let totalKeys = 0;
 
 	function setEntry(inputKey, inputValue) {
 		// Sets entry by using the hashed key as index for the hash map.
@@ -20,14 +20,14 @@ function HashMapFactory() {
 		if (!oldBucket) {
 			// Simply fill the bucket. No Collision
 			hashMap[hashCode] = bucket;
-			filledBuckets++;
+			totalKeys++;
 		} else if (oldBucket.key === inputKey) {
 			// Overwrites old value.
 			oldBucket.value = inputValue;
 		} else {
 			// Appends to ".next" pointer.
 			appendToList(oldBucket, bucket);
-			filledBuckets++;
+			totalKeys++;
 		}
 
 		// Grows the hash map if current load surpasses the load factor (75%);
@@ -52,6 +52,58 @@ function HashMapFactory() {
 		}
 	}
 
+	function hasEntry(inputKey) {
+		if (typeof inputKey !== 'string') throw Error('Key must be a string!');
+
+		if (getValue(inputKey)) return true;
+		return false;
+	}
+
+	function removeEntry(inputKey) {
+		// Removes entry if found and returns true, else returns false.
+		if (typeof inputKey !== 'string') throw Error('Key must be a string!');
+
+		const hashCode = hash(inputKey),
+			head = hashMap[hashCode];
+
+		if (!head) return false;
+		let prevNode = head;
+		return traverseList(head);
+
+		function traverseList(node) {
+			// Removes entry node from linked list, returns true or false.
+			if (!node) return false;
+
+			if (node.key === inputKey) {
+				if (node === head) hashMap[hashCode] = head.next;
+				else prevNode.next = node.next;
+				return true;
+			} else {
+				prevNode = node;
+				return traverseList(node.next);
+			}
+		}
+	}
+
+	function getLength() {
+		return totalKeys;
+	}
+
+	function getAllKeys() {
+		return getAllFilledNodes(bucket => bucket.key);
+	}
+
+	function getAllEntries() {
+		return getAllFilledNodes(bucket => [bucket.key, bucket.value]);
+	}
+
+	function clearAll() {
+		hashMap.length = 0;
+		hashMap.length = 16;
+		totalKeys = 0;
+	}
+
+	// Private Functions
 	function hash(inputStr) {
 		// Returns a hash number after parsing through all char of input string.
 		// This hash number is converted to be able fit in the hash map using modulo operator.
@@ -67,7 +119,7 @@ function HashMapFactory() {
 	}
 
 	function getCurrentLoad() {
-		return (filledBuckets / hashMap.length).toFixed(2);
+		return (totalKeys / hashMap.length).toFixed(2);
 	}
 
 	function growHashMap() {
@@ -81,8 +133,32 @@ function HashMapFactory() {
 		else appendToList(start.next, inputVal);
 	}
 
+	function getAllFilledNodes(format = node => node) {
+		const keys = [];
+		hashMap.forEach(bucket => {
+			if (!bucket) return;
+
+			if (bucket.next) traverseList(bucket);
+			else keys.push(format(bucket));
+		});
+
+		return keys;
+
+		function traverseList(node) {
+			if (!node) return;
+			keys.push(format(node));
+			traverseList(node.next);
+		}
+	}
+
 	return {
 		setEntry,
 		getValue,
+		hasEntry,
+		removeEntry,
+		getLength,
+		getAllKeys,
+		getAllEntries,
+		clearAll,
 	};
 }
